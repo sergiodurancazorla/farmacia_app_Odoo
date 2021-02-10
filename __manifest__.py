a728a369567fd11845d3700d25cd2644b8dda647
sergiodurancazorla <sergiodurancazorla@gmail.com>
2021-02-10 08:43:14 +0100

    Primer commit, estructura del proyecto

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Farmacia y Parafarmacia APP',
+    'description': 'Control de almacen, gestion de pedidos y productos farmaceuticos y de parafarmacia',
+    'author': 'Sergio Duran, Sara Andres, Sergio Cifre',
+    'depends': ['base'],
+    'application': True,
+    'data': [
+
+    ],
+
+}
