19f64c2d231d2bbd061061858e22cd5f10e1e35f
sergiodurancazorla <sergiodurancazorla@gmail.com>
2021-02-10 10:11:56 +0100

    Se añade vista de formulario de producto, permisos en security (falta cambiar estilo de la vista, se ve un poco fea).

--- __manifest__.py
+++ __manifest__.py
@@ -7,6 +7,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/farmacia_menu.xml',
+        'views/producto_view.xml',
 
     ],
 
--- models/farmacia_producto.py
+++ models/farmacia_producto.py
@@ -4,6 +4,7 @@ from odoo import fields, models
 class Producto(models.Model):
     _name = 'farmacia.producto'
     _description = 'Producto'
+
     name = fields.Char('Nombre',
                        default=None,
                        index=True,
@@ -12,11 +13,11 @@ class Producto(models.Model):
                        required=True,
                        translate=False, )
 
-    codigo_nacional = fields.Integer
-    descripcion = fields.Text
-    inventario = fields.Integer
+    codigo_nacional = fields.Integer()
+    descripcion = fields.Text()
+    inventario = fields.Integer()
 
     # atributos relacionados, rellenar más adelante
-    proveedor = fields.Text
-    coste_proveedor = fields.Integer
-    coste_venta = fields.Integer
+    proveedor = fields.Text()
+    coste_proveedor = fields.Integer()
+    coste_venta = fields.Integer()
