70906847e840e7a78f16cc8601d06de35d7bf6cf
Sara <saraclasedam@gmail.com>
2021-02-17 10:26:18 +0100

    ya se ve proveedores

--- models/farmacia_pedidoscompra.py
+++ models/farmacia_pedidoscompra.py
@@ -4,11 +4,10 @@ from odoo import fields, models
 class Pedidoscompra(models.Model):
     _name = 'farmacia.pedidoscompra'
     _description = 'pedidoscompra'
-    _order = 'estado asc'
 
     descripcion = fields.Text('descripcion')
     # lista_productos = fields.Text('lista')
-    filtro_estado = fields.Selection([('pendiente', 'Pendiente de pago'), ('pagado', 'Pagado')], 'estado')
+    # filtro_estado = fields.Selection([('pendiente', 'Pendiente de pago'), ('pagado', 'Pagado')], 'estado')
     coste_pedido = fields.Integer('coste')
 
     proveedor = fields.One2many(
