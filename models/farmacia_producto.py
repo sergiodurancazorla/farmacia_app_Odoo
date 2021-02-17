ede592c73542556e9eb24eebd61f5b60c640c99d
sergiodurancazorla <sergiodurancazorla@gmail.com>
2021-02-17 10:36:26 +0100

    Arreglado error del campo calculado al agregar un nuevo producto

--- models/farmacia_producto.py
+++ models/farmacia_producto.py
@@ -20,7 +20,7 @@ class Producto(models.Model):
         'producto_codigo_nacional',  # nombre del campo en el modelo relacionado
         string='Inventario Total', readonly=True, )
 
-    inventario_total = fields.Integer(compute='_compute_calcularTotal')
+    inventario_total = fields.Integer(compute='_compute_calcularTotal', readonly=True)
 
     codigo_nacional = fields.Integer('Codigo nacional',
                                      default=None,
@@ -40,7 +40,11 @@ class Producto(models.Model):
         if not self.codigo_nacional or len(self.codigo_nacional) != 6:
             raise ValidationError('El codigo nacional debe tener 6 numeros')
 
+    #
     @api.depends('inventario', 'inventario_total')
     def _compute_calcularTotal(self):
-        for record in self.inventario:
-            self.inventario_total = self.inventario_total + record.inventario
+        if not self.inventario :
+            self.inventario_total = 0
+        else:
+            for record in self.inventario:
+                self.inventario_total = self.inventario_total + record.inventario
