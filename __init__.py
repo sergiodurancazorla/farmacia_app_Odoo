8d0ea1c78d4672683fcbcd907914a67e1411828f
sergio <sercif@alu.iesmatisse.es>
2021-02-19 16:50:57 +0100

    Actualizacion de los init con los datos de clientes y pedidosventa. Creación del fichero de datos de clientes y modificacion de accesos

--- __init__.py
+++ __init__.py
@@ -10,3 +10,7 @@ def add_datos_prueba(cr, registry):
 def add_datos_proveedores(cr, registry):
     tools.convert_file(cr, 'farmacia_app_Odoo', 'data/farmacia.proveedores.csv', None, mode='init', noupdate=True,
                        kind='init', report=None)
+
+def add_datos_clientes(cr, registry):
+    tools.convert_file(cr, 'farmacia_app_Odoo', 'data/farmacia.clientes.csv', None, mode='init', noupdate=True,
+                        kind='init', report=None)
--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,5 @@ from . import farmacia_producto
 from . import farmacia_stock
 from . import farmacia_pedidoscompra
 from . import farmacia_proveedores
+from . import farmacia_clientes
+from . import  farmacia_pedidosventa
